74b002cb112ae981c0fad94bfaf8739e225ed4c6
tamura <tamura2004@gmail.com>
2018-04-15 19:14:12 +0900

    Modify make index cave

--- make_index_cave.rb
+++ make_index_cave.rb
@@ -6,9 +6,12 @@ base = Pathname.pwd + "index_cave"
 FileUtils.rm_rf(base) if base.exist?
 # base.mkpath
 
-before = (100000..777776).to_a.sample(2000)
-after = (777778..999999).to_a.sample(6190)
-items = before + [777777] + after
+bootstrap = [500000,400000,900000,350000,450000,700000,950000,600000,800000]
+
+items = (100000..999999).to_a.sample(8181)
+items += [777777]
+items.shuffle!
+items = bootstrap + items
 
 def insert(path,item)
   path.mkpath unless path.exist?
