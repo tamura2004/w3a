6d1d5cc7de21f654aef0f172ade37fa8dad9c811
M.Tamura <tamura2004@gmail.com>
2019-06-07 14:26:12 +0900

    Add postgresql createuser

--- make_user.rb
+++ make_user.rb
@@ -1,12 +1,12 @@
 40.times do |n|
 	name = "a%03d" % n
-# 	puts `createuser -d #{name}`
-	`sudo ln -s /home/tamura/w3a/index_cave /home/#{name}/index_cave`
-	`sudo ln -s /home/tamura/w3a/binary_sea /home/#{name}/binary_sea`
-	`sudo ln -s /home/tamura/w3a/liner_sea /home/#{name}/liner_sea`
-	`sudo ln -s /home/tamura/w3a/hash_sea /home/#{name}/hash_sea`
-	`sudo ln -s /home/tamura/w3a/collision /home/#{name}/collision`
-	`sudo ln -s /home/tamura/w3a/golden_statue /home/#{name}/golden_statue`
+	# `sudo ln -s /home/tamura/w3a/index_cave /home/#{name}/index_cave`
+	# `sudo ln -s /home/tamura/w3a/binary_sea /home/#{name}/binary_sea`
+	# `sudo ln -s /home/tamura/w3a/liner_sea /home/#{name}/liner_sea`
+	# `sudo ln -s /home/tamura/w3a/hash_sea /home/#{name}/hash_sea`
+	# `sudo ln -s /home/tamura/w3a/collision /home/#{name}/collision`
+	# `sudo ln -s /home/tamura/w3a/golden_statue /home/#{name}/golden_statue`
+		puts `createuser -d -s #{name}`
 end
 
 __END__
